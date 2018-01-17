c4c7513374babda7d4a0a1438ff95f826e3185ec
CloudZun <info@cloudzun.com>
2018-01-17 11:05:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Vini Vidi Vici !");
+    response.end("change the webhook address and test again !");
 
 });
 
