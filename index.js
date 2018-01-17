7d7232c3a21c633b20380021306175d4271a1063
CloudZun <info@cloudzun.com>
2018-01-17 14:53:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Good good study,day day up!");
+    response.end("yes boss!");
 
 });
 
