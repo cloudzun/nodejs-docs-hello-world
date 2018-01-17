f1bdd93426cc09333c17982bd5db01a54a9520c2
CloudZun <info@cloudzun.com>
2018-01-17 11:20:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("change the webhook address and test again !");
+    response.end("测试一下中文显示 !");
 
 });
 
