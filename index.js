c3f05eb2aef8163599f8c2b0e71b779795313aae
CloudZun <info@cloudzun.com>
2018-01-17 11:24:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("测试一下中文显示 !");
+    response.end("测试一下中文显示，难道不行……");
 
 });
 
