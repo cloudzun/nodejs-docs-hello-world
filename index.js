8eb2b9473abfa7618a582d95b010205fdfbdf0d4
CloudZun <info@cloudzun.com>
2019-04-20 21:20:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("yes boss!");
+    response.end("Hi , Abraham !");
 
 });
 
