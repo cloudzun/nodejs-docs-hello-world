1d0f9c2e99942d17d0ee8f6ee6f3df6bde655628
CloudZun <info@cloudzun.com>
2020-09-24 12:11:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World from Jenkins + Docker + Github by Zunhua @20200924 13.52 ver4.3 ");
+    response.end("Hello World from Jenkins + Docker + Github by Zunhua @20200924 12:11 ver4.3 ");
 
 });
 
